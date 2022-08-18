cab61b9308d469dfba36c4c29299a22ad98c71ae
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-08-18 20:02:18 +0800

    Update GET.py

--- api/GET.py
+++ api/GET.py
@@ -9,13 +9,17 @@ class handler(BaseHTTPRequestHandler):
     db = client['NRICM101-map']
     col = db['source']
 
+    print(col)
 
-    self.send_response(200)
-    self.send_header('Content-type', 'text/html')
-    self.end_headers()
+    # self.send_response(200)
+    # self.send_header('Content-type', 'text/html')
+    # self.end_headers()
     
 
 
-    self.wfile.write(html.encode('utf-8'))
+    # self.wfile.write(html.encode('utf-8'))
 
     return
+
+if __name__ == '__main__':
+    handler.do_GET()

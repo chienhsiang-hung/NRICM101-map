fd37cc0d65b0d0550308c092fce0bf627eabbf7f
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-08-18 20:28:28 +0800

    Update GET.py

--- api/GET.py
+++ api/GET.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 import pymongo
 import os
+from bson.json_util import dumps
 
 class handler(BaseHTTPRequestHandler):
 
@@ -9,7 +10,7 @@ class handler(BaseHTTPRequestHandler):
     db = client['NRICM101-map']
     col = db['source']
 
-    print(col)
+    print(dumps(list(col.find()), ensure_ascii=False).encode('utf8').decode())
 
     # self.send_response(200)
     # self.send_header('Content-type', 'text/html')
@@ -20,7 +21,4 @@ class handler(BaseHTTPRequestHandler):
     # self.wfile.write(html.encode('utf-8'))
 
     return
-
-if __name__ == '__main__':
-    handler().do_GET()
     

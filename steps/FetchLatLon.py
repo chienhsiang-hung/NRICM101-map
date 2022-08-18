64441f446b7b3d1c18137e31ef3acc6903e19cb3
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-08-18 18:46:11 +0800

    Create FetchLatLon.py

---
+++ steps/FetchLatLon.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from tqdm import tqdm
+import geocoder
+
+# Fetch data from gov
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQjf_HNeEZKM-XJX-q5v4cfNrB3kcv4gOT8kFbV9rurfoX_H5Qv9112Pv0PgYNFSzbReyNlQkLrJib3/pubhtml'
+resp = requests.get(url).text
+soup = BeautifulSoup(resp, 'html.parser')
+
+# parse data from html table to DF
+DF = pd.read_html(str(soup.table), encoding='utf-8')[0]
+DF.columns = pd.MultiIndex.from_arrays(DF.iloc[0:2].values)
+DF = DF.iloc[3:, 1:]
+DF = DF.set_index((None, '編號'))
+
+# address to latlng
+address_list = DF.iloc[:, -2].to_list()
+latlng_list = []
+for address in tqdm(address_list):
+    geo = geocoder.arcgis(address).json
+    latlng_list.append( geo )
+DF[('LatLon', 'LatLon')] = latlng_list
+
+# drop na and retrieve only latlng from geo
+print(f'len b/f cleaning: {len(DF)}')
+for _, row in DF.iterrows():
+    print(row)
+    if not row.to_list()[-1]:
+        DF.drop(index=row.name, inplace=True)
+    else:
+        DF.loc[row.name, ('LatLon', 'LatLon')] = [row[('LatLon', 'LatLon')]['lat'], row[('LatLon', 'LatLon')]['lng']]
+print(f'len a/f cleaning: {len(DF)}')
+
+# sum the ppl
+DF[('剩餘人次', '剩餘人次')] = DF.iloc[:, 2:13].sum(axis=1)
+DF[('剩餘人次', '剩餘人次')] = DF[('剩餘人次', '剩餘人次')].apply(lambda cell: f'{cell:.0f}')
+
+
+DF.columns = DF.columns.map(str)
